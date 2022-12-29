406b2f8cd304b68578d8bb532f35e07996744742
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2022-12-29 16:19:54 -0300

    Desafio imutabilidade

---
+++ Udemy/Cod3r/programacao_funcional/imutabilidade.py
@@ -0,0 +1,37 @@
+from locale import setlocale, LC_ALL
+from calendar import mdays, month_name
+from functools import reduce
+
+# Pt- br
+setlocale(LC_ALL, 'pt_BR')
+
+# Procedural
+
+print(f'Meses com 31 dias:')
+
+for mes in range(1, 13):
+    if mdays[mes] == 31:
+        print(f'- {month_name[mes]}')
+
+
+
+
+# Separados
+indices_31 = filter(lambda mes: mdays[mes] if mdays[mes] == 31 else '', range(1, 13))
+meses_nome = map(lambda mes: month_name[mes], indices_31)
+meses_31 = reduce(lambda todos, nome_mes: f'{todos}\n- {nome_mes}', meses_nome, '\nMeses com 31 dias:')
+
+print(meses_31)
+
+# Juntos
+print(
+    reduce(
+        lambda todos, nome_mes: f'{todos}\n- {nome_mes}',
+        map(
+            lambda mes: month_name[mes],
+            filter(
+                lambda mes: mdays[mes] if mdays[mes] == 31 else '', range(1, 13)
+            )
+        ), f'\nMeses com 31 dias:'
+    )
+)
