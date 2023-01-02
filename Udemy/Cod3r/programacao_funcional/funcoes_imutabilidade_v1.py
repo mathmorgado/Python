c78161b8d11c9dea4f88375c9db4a2ed23970cdc
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-01-02 15:53:35 -0300

    Funções de imutabilidade

---
+++ Udemy/Cod3r/programacao_funcional/funcoes_imutabilidade_v1.py
@@ -0,0 +1,14 @@
+from functools import reduce
+from operator import add
+
+valores =[20, 33, 11, 81, 9, 112]
+
+print(f'Lista: {valores}')
+
+# Funções de imutabilidade
+print(f'\nOrdem crescente: {sorted(valores)}')
+print(f'Menor valor: {min(valores)}')
+print(f'Maior valor: {max(valores)}')
+print(f'Lista revertida: {list(reversed(valores))}')
+print(f'Soma da lista(sum): {sum(valores)}')
+print(f'Soma da lista(reduce): {reduce(add, valores)}')
