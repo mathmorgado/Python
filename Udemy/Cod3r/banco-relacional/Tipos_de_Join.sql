ba720f5aaaee105a0707f36e51d1a6d28ebee93d
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-01-26 17:40:16 -0300

    Create Tipos_de_Join.sql

---
+++ Udemy/Cod3r/banco-relacional/Tipos_de_Join.sql
@@ -0,0 +1,7 @@
+-- Inner join: pega a intersecção de duas tabelas.
+
+-- Left join: pega a intersecção e o outer (externo) da tabela à 'esquerda'.
+
+-- Right join: pega a intersecção e o outer da tabela à 'direita'.
+
+--Full join: Pega tanto a intersecção quanto os outer de ambas as tabelas. (Não suportado pelo SQL)
