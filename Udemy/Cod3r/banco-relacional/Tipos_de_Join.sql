fc35aacfa148b31968b9a62ca32b48d23b2e48cc
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-01-27 19:19:04 -0300

    tipos join

--- Udemy/Cod3r/banco-relacional/Tipos_de_Join.sql
+++ Udemy/Cod3r/banco-relacional/Tipos_de_Join.sql
@@ -1,7 +1,17 @@
+select * from cidades;
+select * from prefeitos;
+
 -- Inner join: pega a intersecção de duas tabelas.
+select * from cidades c inner join prefeitos p on c.id = p.cidade_id;
 
 -- Left join: pega a intersecção e o outer (externo) da tabela à 'esquerda'.
+select * from cidades c left join prefeitos p on c.id = p.cidade_id;
 
 -- Right join: pega a intersecção e o outer da tabela à 'direita'.
+select * from cidades c right join prefeitos p on c.id = p.cidade_id;
 
 --Full join: Pega tanto a intersecção quanto os outer de ambas as tabelas. (Não suportado pelo SQL)
+-- select * from cidades c full join prefeitos p on c.id = p.cidade_id; /Alternativa -> Usar union no right e left join
+select * from cidades c left join prefeitos p on c.id = p.cidade_id
+union
+select * from cidades c right join prefeitos p on c.id = p.cidade_id;
