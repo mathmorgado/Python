880f2c51814e555f5110b9f6c4e518ac9ff001a9
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-02-02 17:57:25 -0300

    Create listar_tabelas.py

---
+++ Udemy/Cod3r/banco_dados/listar_tabelas.py
@@ -0,0 +1,12 @@
+from bd import nova_conexao
+from mysql.connector import ProgrammingError
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute('SHOW TABLES')
+
+        for i, tables in enumerate(cursor, start=1):
+            print(f'Tabela {i} - {tables[0]}')
+    except ProgrammingError as e:
+        print(f'ERRO? {e.msg}')
