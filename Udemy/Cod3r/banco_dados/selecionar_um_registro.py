bd4159abe47c59d94b1a05763ed0cb85aa0c4062
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-02-03 14:45:29 -0300

    Update selecionar_um_registro.py

--- Udemy/Cod3r/banco_dados/selecionar_um_registro.py
+++ Udemy/Cod3r/banco_dados/selecionar_um_registro.py
@@ -1,12 +1,11 @@
 from bd import nova_conexao
 
-sql = 'SELECT * FROM contatos'
+sql = 'SELECT tel, nome FROM contatos LIMIT 10 OFFSET 10'
 
 with nova_conexao() as conexao:
     cursor = conexao.cursor()
     cursor.execute(sql)
 
-    print(cursor.fetchone())
     print(cursor.fetchone())
     print(cursor.fetchone())
     print(cursor.fetchone())
