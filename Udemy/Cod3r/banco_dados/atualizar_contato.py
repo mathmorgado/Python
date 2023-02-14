6365b84b83fbb78e990ed9a0e81cfda37633caf3
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-02-14 18:52:45 -0300

    change atualizar_contatos.py

--- Udemy/Cod3r/banco_dados/atualizar_contato.py
+++ Udemy/Cod3r/banco_dados/atualizar_contato.py
@@ -1,8 +1,8 @@
 from bd import nova_conexao
 from mysql.connector.errors import ProgrammingError
 
-sql = 'UPDATE nome FROM contatos WHERE id = %s'
-args = (input('Digite o ID para atualizar o nome: '),)
+sql = 'UPDATE contatos set nome = %s WHERE id = %s'
+args = (input('Digite o NOME de atualização: '), input('Digite o ID que deseja atualizar: '))
 
 with nova_conexao() as conexao:
     try:
---
+++ Udemy/Cod3r/banco_dados/selecionar_contatos_limit_offset.py
@@ -0,0 +1,15 @@
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = 'SELECT * FROM contatos'
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+        contatos = cursor.fetchall() # Cuidado caso tenha muitos dados na tabela!
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
+    else:
+        for contato in contatos:
+            print(f'{contato[2]:3d} - {contato[0]:7s} Telefone: {contato[1]}')
