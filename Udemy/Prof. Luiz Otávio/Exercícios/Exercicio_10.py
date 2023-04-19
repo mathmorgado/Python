e35f90c38da808b756f76a46aacce426265d5e27
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-19 13:32:54 -0300

    Create Exercicio_10.py

---
+++ Udemy/Prof. Luiz Otávio/Exercícios/Exercicio_10.py
@@ -0,0 +1,27 @@
+frase = 'O Rato roeu a roupa do Rei de roma, mas o rei de roma não roeu a roupa do rato'
+
+i = 0
+while i < len(frase):
+    letra = frase.lower()[i]
+    letra_mais_aperecida = None
+    qtde_letra = 0
+    print(letra)
+
+    if i == 0:
+        letra_mais_aperecida = letra
+        qtde_letra = frase.lower().count(letra)
+        i += 1
+        continue
+    
+    if letra == ' ':
+        i += 1
+        continue
+    
+    if frase.lower().count(letra) > qtde_letra:
+        letra_mais_aperecida = letra
+        qtde_letra = frase.lower().count(letra)
+
+    i += 1
+
+print(f'A letra mais aparecida foi "{letra_mais_aperecida.upper()}", tendo aparecido {qtde_letra} vezes!')
+
