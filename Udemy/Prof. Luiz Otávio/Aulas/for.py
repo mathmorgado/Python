829034f3a0fc773c5d29a3c23cc57be901508e37
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-20 15:20:25 -0300

    Update for.py

--- Udemy/Prof. Luiz Otávio/Aulas/for.py
+++ Udemy/Prof. Luiz Otávio/Aulas/for.py
@@ -26,3 +26,18 @@ while True: # Repetição do processo
     except StopIteration: # Erro de interation quando tenta buscar um proximo valor que nn existe
         break
 
+# Aplicações para o 'for'
+
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue # Voltar ao início do laço ignorando os códigos abaixo
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break # Quebrar o laço, saindo dele ignorando os códigos abaixo inclusive o else!
+
+    for j in range(1, 3): # Laço 'for' dentro de outro laço 'for'
+        print(i, j)
+else: # Executado após o fim do laço, caso não tenha break
+    print('For completo com sucesso!')
