97af829f44c256d3d085aa901245a5f5f171c664
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-24 14:53:33 -0300

    Create Exercício_12.py

---
+++ Udemy/Prof. Luiz Otávio/Exercícios/Exercício_12.py
@@ -0,0 +1,50 @@
+"""
+Exercício - Lista de Compras
+
+[x] Criar menu de opções com: Inserir, Apagar e Listar
+[x] Construir a funcionalidade das opções
+[x] Exibir o índice e o valor da lista
+[x] Tratar erros
+"""
+
+lista_compras = []
+qtde = None
+
+while True:
+    opcao = str(input('\nSelecione uma opção\n[i]nserir [a]pagar [l]istar [s]air: ')).lower().strip()
+
+    if not opcao.isalpha():
+        print('\n\033[31mError: Opção inválida!\033[m\n')
+        continue
+    
+    if opcao == 'i':
+        item = str(input('Item que deseja inserir: '))
+        lista_compras.append(item)
+    
+    elif opcao == 'a':
+        try:
+            item = int(input('índice do item que deseja apagar: '))
+            lista_compras.pop(item)
+        except:
+            print('\n\033[31mError: Opção inválida!\033[m\n')
+            continue
+    
+    elif opcao == 'l':
+        print()
+
+        print(32*'~')
+        print('LISTA DE COMPRAS'.center(32))
+        print(32*'~')
+
+        for indice, item in enumerate(lista_compras):
+            print(indice, item)
+
+        print()
+    
+    elif opcao == 's':
+        print('SAINDO...')
+        break
+
+    else:
+        print('\n\033[31mError: Opção inválida!\033[m\n')
+        continue
