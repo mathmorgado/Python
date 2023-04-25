5cedd5f74a6aad690ddce58103d0cd41cb1166b5
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-25 15:05:18 -0300

    Create operacao_ternaria.py

---
+++ Udemy/Prof. Luiz Otávio/Aulas/operacao_ternaria.py
@@ -0,0 +1,14 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+
+condicao = 10 == 11
+variavel = 'Valor' if condicao else 'Outro valor'
+print(variavel)
+
+digito = 9
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
+
+print('Valor' if False else 'Outro valor' if True else 'Fim')
