70dedf9acab5cb0501829ac1515ad4a93e0841e5
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-25 14:07:59 -0300

    Update Exercício_12.py

--- Udemy/Prof. Luiz Otávio/Exercícios/Exercício_12.py
+++ Udemy/Prof. Luiz Otávio/Exercícios/Exercício_12.py
@@ -6,7 +6,7 @@ Exercício - Lista de Compras
 [x] Exibir o índice e o valor da lista
 [x] Tratar erros
 """
-
+import os
 lista_compras = []
 
 while True:
@@ -15,16 +15,20 @@ while True:
     if opcao == 'i':
         item = str(input('Item que deseja inserir: '))
         lista_compras.append(item)
+        os.system('cls')
     
     elif opcao == 'a':
         try:
             item = int(input('índice do item que deseja apagar: '))
             lista_compras.pop(item)
+            os.system('cls')
         except:
+            os.system('cls')
             print('\n\033[31mError: Opção inválida!\033[m\n')
             continue
     
     elif opcao == 'l':
+        os.system('cls')
         print()
 
         print(32*'~')
@@ -38,8 +42,10 @@ while True:
     
     elif opcao == 's':
         print('SAINDO...')
+        os.system('cls')
         break
 
     else:
+        os.system('cls')
         print('\n\033[31mError: Opção inválida!\033[m\n')
         continue
