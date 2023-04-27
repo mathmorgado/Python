fc6da8b5cadb5e0c267053d2253a4cd14fb63c3a
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-27 14:03:37 -0300

    Create list_comprehension.py

---
+++ Udemy/Prof. Luiz Otávio/Aulas/list_comprehension.py
@@ -0,0 +1,15 @@
+# Introdução à List comprehension em Python
+# List comprehension é uma forma rápida para criar listas
+# a partir de iteráveis.
+
+#print(list(range(10)))
+
+lista = []
+for numero in range(10):
+    lista.append(numero)
+#print(lista)
+
+# Exatamente iguais!
+
+lista = [numero * 2 for numero in range(10)]
+
