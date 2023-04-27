281817d6805f30ca3d0b945f5f4cedf01dcb9b8a
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-27 15:10:14 -0300

    Create Iterator.py

---
+++ Udemy/Prof. Luiz Otávio/Aulas/Iterator.py
@@ -0,0 +1,3 @@
+# Generator expression, Iterables e Iterators em Python
+iterable = ['Eu', 'tenho', '__iter__']
+iterator = iter(iterable) #  tem __iter__ e __next__ /iterator só sabe qual é o próximo valor do iteravél
