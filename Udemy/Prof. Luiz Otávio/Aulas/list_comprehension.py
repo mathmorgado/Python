ec6d66959838d97b8421923b8a519ac48211addc
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-27 14:20:24 -0300

    Update list_comprehension.py

--- Udemy/Prof. Luiz Otávio/Aulas/list_comprehension.py
+++ Udemy/Prof. Luiz Otávio/Aulas/list_comprehension.py
@@ -29,5 +29,13 @@ novos_produtos = [
 
 print(*novos_produtos, sep='\n')
 
+# lista = []
+# for x in range(3):
+#     for y in range(3):
+#         print((x, y))
+# print(f'List com 2 for: {lista}')
 
+# lista = [(x, y) for x in range(3) for y in range(3)] -> list comprehension com 2 for
 
+lista = [[(x, y) for y in range(3)] for x in range(3)]
+print(f'List comprehension dentro de outra list comprehension: {lista}')
