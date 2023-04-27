34d77533f58c4459dad80e7d650cda394ab0090d
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-27 15:17:26 -0300

    Update Iterator.py

--- Udemy/Prof. Luiz Otávio/Aulas/Iterator.py
+++ Udemy/Prof. Luiz Otávio/Aulas/Iterator.py
@@ -1,3 +1,11 @@
 # Generator expression, Iterables e Iterators em Python
 iterable = ['Eu', 'tenho', '__iter__']
 iterator = iter(iterable) #  tem __iter__ e __next__ /iterator só sabe qual é o próximo valor do iteravél
+
+# 'for' na mão
+while True:
+    try:
+        print(next(iterator))
+    except StopIteration:
+        break
+
