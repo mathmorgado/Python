aa1fbf0ace68463fd8f3769ef43c8131ccffa31b
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-28 14:33:30 -0300

    Update generator.py

--- Udemy/Prof. Luiz Otávio/Aulas/generator.py
+++ Udemy/Prof. Luiz Otávio/Aulas/generator.py
@@ -1,8 +1,22 @@
 # Genarator é uma função que pausa, frequentemente usado para produzir uma grande sequência de valores, mas sem armazenar tudo na memória de uma vez
 import sys
 
-generator = (n for n in range(1000000))
+generatorr = (n for n in range(1000000))
 lista = [n for n in range(1000000)]
 
-print(sys.getsizeof(lista))
-print(sys.getsizeof(generator))
+# print(sys.getsizeof(lista)) -> 8448728kb
+# print(sys.getsizeof(generatorr)) -> 11kb
+
+def generator(n=0, maximum=10):
+    while True:
+        yield n
+        n += 1
+
+        if n > maximum:
+            return n
+
+        
+gen = generator(n=0)
+
+for n in gen:
+    print(n)
