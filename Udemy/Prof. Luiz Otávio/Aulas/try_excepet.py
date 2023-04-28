40f6f0e536ac8876cf989937da8a7c2473418ed6
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-28 14:50:42 -0300

    Update try_excepet.py

--- Udemy/Prof. Luiz Otávio/Aulas/try_excepet.py
+++ Udemy/Prof. Luiz Otávio/Aulas/try_excepet.py
@@ -22,4 +22,24 @@ except (TypeError, IndexError) as error:
 except Exception:
     print('ERRO DESCONHECIDO.')
 
-print('CONTINUAR')
+print('CONTINUA')
+
+# else / finally
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
+try:
+    print('ABRIR ARQUIVO')
+    8/0
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
+    print('DIVIDIU ZERO')
+except IndexError as error:
+    print('IndexError')
+except (NameError, ImportError):
+    print('NameError, ImportError')
+else: # Se nn dar erro o finaly é executado
+    print('Não deu erro')
+finally: # Sempre será executado, independente de haver um erro ou nn
+    print('FECHAR ARQUIVO')
+
+    
