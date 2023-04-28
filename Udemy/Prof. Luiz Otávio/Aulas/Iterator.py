d272a934e662186e70bb63c72ec7edae37e30815
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-04-28 14:11:01 -0300

    iterator

--- Udemy/Prof. Luiz Otávio/Aulas/Iterator.py
+++ Udemy/Prof. Luiz Otávio/Aulas/Iterator.py
@@ -1,6 +1,6 @@
 import sys
 
-# Generator expression, Iterables e Iterators em Python
+# Iterables e Iterators em Python
 iterable = ['Eu', 'tenho', '__iter__']
 iterator = iter(iterable) #  tem __iter__ e __next__ /iterator só sabe qual é o próximo valor do iteravél
 
@@ -11,8 +11,3 @@ while True:
     except StopIteration:
         break
 
-generator = (n for n in range(1000000))
-lista = [n for n in range(1000000)]
-
-print(sys.getsizeof(lista))
-print(sys.getsizeof(generator))
