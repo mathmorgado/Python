9151e3a0c4e364e0fd27c8314dc2775dab4fc8fd
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-05-02 15:44:00 -0300

    exercicio 18

---
+++ Udemy/Prof. Luiz Otávio/Exercícios/Exercicio_18_v2.py
@@ -0,0 +1,20 @@
+"""
+Considerando duas listas de inteiros ou floats (lista A e lista B)
+Some os valores nas listas retornando uma nova lista com os valores somados:
+Se uma lista for maior que a outra, a soma só vai considerar o tamanho da
+menor.
+Exemplo:
+lista_a     = [1, 2, 3, 4, 5, 6, 7]
+lista_b     = [1, 2, 3, 4]
+=================== resultado
+lista_soma  = [2, 4, 6, 8]
+"""
+
+def zip_soma(lista_1, lista_2):
+    intervalo_maximo = min(len(lista_1), len(lista_2))
+    return [lista_1[i] + lista_2[i] for i in range(intervalo_maximo)]
+
+lista_a = [1, 2, 3, 4, 5, 6, 7]
+lista_b = [1, 2, 3, 4]
+
+print(zip_soma(lista_a, lista_b))
