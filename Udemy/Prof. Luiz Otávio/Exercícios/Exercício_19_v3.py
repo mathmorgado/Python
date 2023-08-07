597b6a6eaed7169c216949f4c7eae80aae4fbfe7
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-08-07 11:33:07 -0300

    edit

--- Udemy/Prof. Luiz Otávio/Exercícios/Exercício_19_v3.py
+++ Udemy/Prof. Luiz Otávio/Exercícios/Exercício_19_v3.py
@@ -1,13 +1,5 @@
-# Exercício - Lista de tarefas com desfazer e refazer
-# Música para codar =)
-# Everybody wants to rule the world - Tears for fears
-# todo = [] -> lista de tarefas
-# todo = ['fazer café'] -> Adicionar fazer café
-# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
-# desfazer = ['fazer café',] -> Refazer ['caminhar']
-# desfazer = [] -> Refazer ['caminhar', 'fazer café']
-# refazer = todo ['fazer café']
-# refazer = todo ['fazer café', 'caminhar']
+# Exercício - Lista de tarefas com desfazer e refazer EVITANDO IF'S
+
 from os import system
 
 tarefas = []
