2b00ee313396b4615de1f1ac5ab67a24c9137d65
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-05 14:00:48 -0300

    Update Tic-Tac-Toe Checker.py

--- Self/CodeWars/Tic-Tac-Toe Checker.py
+++ Self/CodeWars/Tic-Tac-Toe Checker.py
@@ -4,25 +4,42 @@ board = [
     [1, 2, 1],
 ]
 
-hori_win = False
-vert_win = False
-diag_win = False
+hori_win = True
+vert_win = True
+diag_win = True
+
+stop_hori = False
+stop_vert = False
+
+winner = None
 
 found_zero = False
 for i in range(len(board)):
+    first = board[i][0]
 
     for j in range(len(board[i])):
-        current = board[i][j]
+        current_hori = board[i][j]
+        current_vert = board[j][i]
 
-        hori_win = (board[i][j] == current)
-        vert_win = (board[j][i] == current)
+        if board[i][j] == 0:
+            found_zero = True
 
-    diag_win = (board[i][i] == current)
+        if (current_hori != first or stop_hori):
+            hori_win = False
+            stop_hori = True
 
-if board[0][0] == 1 and board[1][0] == 1 and board[2][0] == 1:
-    print("X win")
-elif board[0][0] == 2 and board[1][0] == 2 and board[2][0] == 2:
-    print("O win")
+        if (current_vert != first or stop_vert):
+            vert_win = False
+            stop_vert = True
+
+    if hori_win or vert_win:
+        winner = first
+        break
+
+    diag_win = (board[i][i] == first)
+
+if hori_win or vert_win or diag_win:
+    print(winner)
 elif found_zero:
     print("Not finished")
 else:
