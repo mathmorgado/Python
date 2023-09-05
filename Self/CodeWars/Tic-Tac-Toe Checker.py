435d7618714e521e4077d661ffbb5b270dd6e054
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-05 13:32:00 -0300

    Update Tic-Tac-Toe Checker.py

--- Self/CodeWars/Tic-Tac-Toe Checker.py
+++ Self/CodeWars/Tic-Tac-Toe Checker.py
@@ -4,13 +4,16 @@ board = [
     [1, 2, 1],
 ]
 
-hori_x = 0
-vert_y = 0
 found_zero = False
-
 for i in range(len(board)):
-    for j in board[i]:
-        if j == 0:
-            found_zero = True
-        hori_x = j
-        vert_y = j
+    for j in range(len(board[i])):
+        
+
+if board[0][0] == 1 and board[1][0] == 1 and board[2][0] == 1:
+    print("X win")
+elif board[0][0] == 2 and board[1][0] == 2 and board[2][0] == 2:
+    print("O win")
+elif found_zero:
+    print("Not finished")
+else:
+    print("Tie!")
