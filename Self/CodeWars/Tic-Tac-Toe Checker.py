7e1be8fb35d761f8ff473637876ed5cf1ae7ca45
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-05 15:31:52 -0300

    Update Tic-Tac-Toe Checker.py

--- Self/CodeWars/Tic-Tac-Toe Checker.py
+++ Self/CodeWars/Tic-Tac-Toe Checker.py
@@ -1,91 +1,34 @@
 board = [
-    [2, 1, 2],
-    [2, 1, 1],
     [1, 1, 2],
+    [1, 2, 1],
+    [2, 1, 2],
 ]
-
-hori_win = False
-vert_win = False
-diag_win = False
-invert_diag_win = False
-
-winner = None
 
 found_zero = False
 
-for i in range(len(board)):
-    first_hori = board[i][0]
-    first_vert = board[0][i]
-    first_diag = board[0][0]
-    invert_first = board[0][-1]
+for i in range(3):
+    if board[i][0] == board[i][1] == board[i][2] != 0:
+        print(f"O vencedor é o jogador {board[i][0]}")
+        exit()
 
-    current_hori = None
-    current_vert = None
-    current_diag = None
-    invert_current_diag = None
+for i in range(3):
+    if board[0][i] == board[1][i] == board[2][i] != 0:
+        print(f"O vencedor é o jogador {board[0][i]}")
+        exit()
 
-    for h in range(len(board[i])):
-        current_hori = board[i][h]
+if board[0][0] == board[1][1] == board[2][2] != 0:
+    print(f"O vencedor é o jogador {board[0][0]}")
+    exit()
 
-        if current_hori == 0:
-            found_zero = True
-            hori_win = False
-            break
+if board[0][2] == board[1][1] == board[2][0] != 0:
+    print(f"O vencedor é o jogador {board[0][2]}")
+    exit()
 
-        if current_hori != first_hori:
-            hori_win = False
-            break
+for row in board:
+    if 0 in row:
+        found_zero = True
 
-    for v in range(len(board[i])):
-        current_vert = board[v][i]
-
-        if current_vert == 0:
-            found_zero = True
-            vert_win = False
-            break
-
-        if current_vert != first_vert:
-            vert_win = False
-            break
-
-    for d in range(len(board[i])):
-        current_diag = board[d][d]
-
-        if current_diag == 0:
-            found_zero = True
-            diag_win = False
-            break
-
-        if current_diag != first_diag:
-            diag_win = False
-            break
-
-    for i_d in range(len(board[i])):
-        invert_current_diag = board[i_d][-i_d]
-
-        if invert_current_diag == 0:
-            found_zero = True
-            invert_diag_win = False
-            break
-
-        if invert_current_diag != invert_first:
-            invert_diag_win = False
-            break
-
-    if hori_win or vert_win or diag_win or invert_diag_win:
-        if hori_win:
-            winner = first_hori
-        elif vert_win:
-            winner = first_vert
-        elif diag_win:
-            winner = first_diag
-        else:
-            winner = invert_first
-        break
-
-if hori_win or vert_win or diag_win or invert_diag_win:
-    print(winner)
-elif found_zero:
-    print("Not finished")
+if found_zero:
+    print("O jogo ainda não terminou")
 else:
-    print("Tie!")
+    print("O jogo empatou")
