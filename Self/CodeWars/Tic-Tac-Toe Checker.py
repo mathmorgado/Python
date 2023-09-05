233fc7e3a5f7f5dc775de1d1d39b4b8ef1a8b5cf
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-05 15:23:47 -0300

    Update Tic-Tac-Toe Checker.py

--- Self/CodeWars/Tic-Tac-Toe Checker.py
+++ Self/CodeWars/Tic-Tac-Toe Checker.py
@@ -14,64 +14,63 @@ winner = None
 found_zero = False
 
 for i in range(len(board)):
+    first_hori = board[i][0]
+    first_vert = board[0][i]
+    first_diag = board[0][0]
+    invert_first = board[0][-1]
+
+    current_hori = None
+    current_vert = None
+    current_diag = None
+    invert_current_diag = None
 
     for h in range(len(board[i])):
-        first_hori = board[i][0]
         current_hori = board[i][h]
 
-        if board[i][h] == 0:
+        if current_hori == 0:
             found_zero = True
             hori_win = False
             break
 
-        if (current_hori != first_hori):
+        if current_hori != first_hori:
             hori_win = False
             break
 
     for v in range(len(board[i])):
-        first_vert = board[0][v]
         current_vert = board[v][i]
 
-        if board[i][h] == 0:
+        if current_vert == 0:
             found_zero = True
             vert_win = False
             break
 
-        if (current_vert != first_vert):
+        if current_vert != first_vert:
             vert_win = False
             break
-
-        vert_win = True
 
     for d in range(len(board[i])):
-        first_diag = board[0][0]
         current_diag = board[d][d]
 
-        if board[i][h] == 0:
+        if current_diag == 0:
             found_zero = True
             diag_win = False
             break
 
-        if (current_diag != first_diag):
+        if current_diag != first_diag:
             diag_win = False
             break
-
-        diag_win = True
 
     for i_d in range(len(board[i])):
-        invert_first = board[0][-1]
-        invert_current_diag = board[d][-d]
+        invert_current_diag = board[i_d][-i_d]
 
-        if board[i][h] == 0:
+        if invert_current_diag == 0:
             found_zero = True
             invert_diag_win = False
             break
 
-        if (invert_current_diag != invert_first):
+        if invert_current_diag != invert_first:
             invert_diag_win = False
             break
-
-        invert_diag_win = True
 
     if hori_win or vert_win or diag_win or invert_diag_win:
         if hori_win:
