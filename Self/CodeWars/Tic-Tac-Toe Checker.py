3cca7dd3f0f429b8a0f94a77d119d61507431c3d
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-20 16:24:26 -0300

    Update Tic-Tac-Toe Checker.py

--- Self/CodeWars/Tic-Tac-Toe Checker.py
+++ Self/CodeWars/Tic-Tac-Toe Checker.py
@@ -9,22 +9,21 @@ found_zero = False
 # horizontal win
 for i in range(3):
     if board[i][0] == board[i][1] == board[i][2] != 0:
-        print(f"O vencedor é o jogador {board[i][0]}")
+        print(f"The winner is {board[i][0]}")
         exit()
 
 # vertical win
 for i in range(3):
     if board[0][i] == board[1][i] == board[2][i] != 0:
-        print(f"O vencedor é o jogador {board[0][i]}")
+        print(f"The winner is {board[0][i]}")
         exit()
 
-# diagonal win
 if board[0][0] == board[1][1] == board[2][2] != 0:
-    print(f"O vencedor é o jogador {board[0][0]}")
+    print(f"The winner is {board[0][0]}")
     exit()
 
 if board[0][2] == board[1][1] == board[2][0] != 0:
-    print(f"O vencedor é o jogador {board[0][2]}")
+    print(f"The winner is {board[0][2]}")
     exit()
 
 for row in board:
